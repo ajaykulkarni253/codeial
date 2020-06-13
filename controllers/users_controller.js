b906512fdc40ad06a9194f0bbadffbce8678aae6
ajaykulkarni253 <ajaykulkarni253@gmail.com>
2020-06-13 17:06:02 +0530

    User Sign Up completed

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,51 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile',{
         title: "User Profile"
     });
+}
+
+
+//Render the Sign Up Page
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title: "Codeial | Sign Up"
+    });
+}
+
+
+//Render the Sign In Page
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: "Codeial | Sign In"
+    });
+}
+
+//Get the sign up data
+
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user in signing up'); return}
+
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error in creating the user while signing up'); return}
+
+                return res.redirect('/users/sign-in');
+            });
+        }else{
+            return res.redirect('back');
+        }
+    });
+
+}
+
+//Sign In and create a session for the user
+module.exports.createSession = function(req, res){
+    //TODO later
 }
